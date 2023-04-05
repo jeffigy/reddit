acadff7aef59f8cff648c642ff34d225f3f4a58b
jeffigy <evangelistajeffy25@gmail.com>
2023-04-05 20:10:36 +0800

    added other components to user menu

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,10 +1,11 @@
-import { Button, Flex } from "@chakra-ui/react";
+import { Button, Flex, Menu } from "@chakra-ui/react";
 import React from "react";
 import AuthButtons from "./AuthButtons";
 import AuthModal from "../../Modal/Auth/AuthModal";
 import { User, signOut } from "firebase/auth";
 import { auth } from "@/src/firebase/clientApp";
 import Icons from "./Icons";
+import UserMenu from "./UserMenu";
 
 type RightContentProps = {
   user?: User | null;
@@ -16,6 +17,7 @@ const RightContent: React.FC<RightContentProps> = ({ user }) => {
       <AuthModal />
       <Flex justify={"center"} align={"center"}>
         {user ? <Icons /> : <AuthButtons />}
+        <UserMenu user={user} />
       </Flex>
     </>
   );
