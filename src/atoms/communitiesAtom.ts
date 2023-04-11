5f3b64663dcdbc0b09daa962680f762e041f2073
jeffigy <evangelistajeffy25@gmail.com>
2023-04-11 19:36:22 +0800

    added dynamic routing and 404 page

---
+++ src/atoms/communitiesAtom.ts
@@ -0,0 +1,10 @@
+import { Timestamp } from "@google-cloud/firestore";
+import { atom } from "recoil";
+export interface Community {
+  id: string;
+  creatorId: string;
+  numberOfMembers: number;
+  privacyType: "public" | "private" | "restricted";
+  createdAt?: Timestamp;
+  imageURL?: string;
+}
---
+++ src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,40 @@
+import { Community } from "@/src/atoms/communitiesAtom";
+import NotFound from "@/src/components/Community/NotFound";
+import { firestore } from "@/src/firebase/clientApp";
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSidePropsContext } from "next";
+import React from "react";
+import safeJsonStringify from "safe-json-stringify";
+
+type CommunityPageProps = {
+  communityData: Community;
+};
+
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  if (!communityData) return <NotFound />;
+  return <div>welcome to community {communityData.id}</div>;
+};
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // get community data
+  try {
+    const communityDocRef = doc(
+      firestore,
+      "communities",
+      context.query.communityId as string
+    );
+    const communityDoc = await getDoc(communityDocRef);
+    return {
+      props: {
+        communityData: communityDoc.exists()
+          ? JSON.parse(
+              safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+            )
+          : "",
+      },
+    };
+  } catch (error) {
+    //could add error page here
+    console.log("getServerSideProps error", error);
+  }
+}
+export default CommunityPage;
