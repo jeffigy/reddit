71f05c8694750f9624317aabe007645449d9ae78
jeffigy <evangelistajeffy25@gmail.com>
2023-04-13 18:45:30 +0800

    added community page join/joined button state

---
+++ src/hooks/useCommunityData.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import {
+  Community,
+  CommunitySnippet,
+  communityState,
+} from "../atoms/communitiesAtom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, firestore } from "../firebase/clientApp";
+import { collection, getDocs } from "firebase/firestore";
+
+const useCommunityData = () => {
+  const [user] = useAuthState(auth);
+  const [communityStateValue, setCommunityStateValue] =
+    useRecoilState(communityState);
+  const [loading, setLoading] = useState(false);
+  const onJoinOrLeaveCommunity = (
+    communityData: Community,
+    isJoined: boolean
+  ) => {
+    // is the user signed in ?
+    // if not,  open auth modal
+
+    if (isJoined) {
+      leaveCommunity(communityData.id);
+      return;
+    }
+    joinCommunity(communityData);
+  };
+  const getMySnippets = async () => {
+    setLoading(true);
+    try {
+      //get users snippets
+      const snippetDocs = await getDocs(
+        collection(firestore, `users/${user?.uid}/communitySnippets`)
+      );
+      const snippets = snippetDocs.docs.map((doc) => ({
+        ...doc.data(),
+      }));
+      setCommunityStateValue((prev) => ({
+        ...prev,
+        mySnippets: snippets as CommunitySnippet[],
+      }));
+      console.log(snippets);
+    } catch (error) {
+      console.log("getMySnippets error", error);
+    }
+  };
+  const joinCommunity = (communityData: Community) => {};
+  const leaveCommunity = (communityId: string) => {};
+  useEffect(() => {
+    if (!user) return;
+    getMySnippets();
+  }, [user]);
+
+  return {
+    //data and function
+    communityStateValue,
+    onJoinOrLeaveCommunity,
+  };
+};
+export default useCommunityData;
